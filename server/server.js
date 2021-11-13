f7e4f1de8ccd484bb0af8f76624c89cd82b86fbb
J-Whisler <jwhisler521@gmail.com>
2021-11-13 12:25:57 -0500

    routes, models set up for backend sql db

--- server/server.js
+++ server/server.js
@@ -6,7 +6,14 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
+const db = require("./models");
 
-app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
+// Post Router
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`server running on port ${PORT}`)
+    }) 
 })
